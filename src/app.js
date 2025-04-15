72424495fed2de5539fe1d8619ccbc2cfb2d027c
Sergio García <sergio4geeks@gmail.com>
2025-04-15 19:38:30 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+
+  const value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const element = ["♦","♥", "♠", "♣"];
+
+  const randomValue = value[Math.floor(Math.random() * value.length)];
+  const randomElement = element[Math.floor(Math.random () * element.length)];
+
+  
+  document.querySelector("#center").textContent = randomValue;
+  document.querySelector("#left").textContent = randomElement;
+  document.querySelector("#right").textContent = randomElement;
+
+
+  if (randomElement === "♥" || randomElement === "♦") {
+    document.querySelector("#left").style.color = "red";
+    document.querySelector("#right").style.color = "red";
+  }
+
+  
   console.log("Hello Rigo from the console!");
 };
